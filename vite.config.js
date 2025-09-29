365462422a8c6932725ba344bd3bcac73ee79aca
Aman Sharma <aman.sharma@ilink-systems.com>
2025-09-29 12:48:08 +0530

    a

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',           // ensure assets are referenced from root
   server: { port: 3000 }
 })
